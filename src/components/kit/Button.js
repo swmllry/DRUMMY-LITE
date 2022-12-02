d3396155d7ab382b6f5d03a67965b2c17584e274
Scott Mallory <scott.w.mallory@gmail.com>
2022-12-01 19:15:03 -0500

    + .env alternative

--- src/components/kit/Button.js
+++ src/components/kit/Button.js
@@ -35,6 +35,7 @@ const Button = ({ sample, id, url, setDisplay, audioContextState, setCurrentSamp
 
                 setDisplay(sample)
                 setCurrentSample(`${process.env.REACT_APP_PATH_TO_FILES}${url}`)
+                console.log('url: ', url);
 
             }
         };
@@ -81,7 +82,7 @@ const Button = ({ sample, id, url, setDisplay, audioContextState, setCurrentSamp
                     }
 
                     setDisplay(sample)
-                    setCurrentSample(`${process.env.REACT_APP_PATH_TO_FILES}${url}`)
+                    setCurrentSample(`${process.env.REACT_APP_PATH_TO_FILES || ''}${url}`)
 
                 }}
 
